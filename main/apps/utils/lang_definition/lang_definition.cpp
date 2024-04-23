2ffe71e1f72b6e446371455746ffb02b3cfacb7e
Bjarne <basicx.strgv@gmail.com>
2024-04-23 18:40:59 +0200

    Language definition files for the duckling app can now be loaded

--- main/apps/utils/lang_definition/lang_definition.cpp
+++ main/apps/utils/lang_definition/lang_definition.cpp
@@ -13,7 +13,6 @@
 #include <string>
 #include "../json/json.hpp"
 #include "lang_definition.hpp"
-#include "spdlog/spdlog.h"
 
 using namespace DUCKLING::LANGUAGE;
 using json = nlohmann::json;
@@ -22,7 +21,37 @@ void LangDefinition::load(std::string jsonContent)
 {
     json jsonObject = json::parse(jsonContent);
 
-    // TODO
+    for (auto &el : jsonObject.items())
+    {
+        std::string identifier = el.key();
+        std::string value_string = el.value();
+
+        if (identifier == "__comment" || value_string.size() < 8)
+        {
+            // Skip comments and value strings with invalid length
+            continue;
+        }
+
+        try
+        {
+            // Get hex values
+            uint8_t modifier_keys_string = std::stoi(value_string.substr(0, 2), 0, 16);
+            uint8_t reserved = std::stoi(value_string.substr(3, 2), 0, 16);
+            uint8_t keycode = std::stoi(value_string.substr(6, 2), 0, 16);
+
+            CodeInfo_t codeinfo;
+            codeinfo.modifier_keys = modifier_keys_string;
+            codeinfo.reserved = reserved;
+            codeinfo.keycode = keycode;
+
+            _keymap[identifier] = codeinfo;
+        }
+        catch (...)
+        {
+            // Skip enty on error
+            continue;
+        }
+    }
 }
 
 CodeInfo_t LangDefinition::get_code_info(std::string identifier)
